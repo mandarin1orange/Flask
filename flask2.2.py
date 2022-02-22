32acd35e362462ac0b76715fa8294dfa583a8c25
mandarin1orange <100086932+mandarin1orange@users.noreply.github.com>
2022-02-22 22:51:44 +0800

    flask的请求和参数
    POST传参

---
+++ flask2.2.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import flask
+from flask import Flask
+from flask import request
+
+# 创建flask程序
+app = Flask(__name__,
+            static_url_path='/static',  # 静态文件路径
+            static_folder='static',
+            template_folder='templates'  # 模板文件
+            )
+
+@app.route('/a',methods=['POST'])
+def a_page():
+    user_name = request.form.get('uname')
+    user_pass = request.form.get('upass')
+    return 'this is a page %s --- %s hahahaha'%(user_name,user_pass)
+    pass
+
+# 装饰器，关联路由
+@app.route('/')
+def index():
+    return 'haha'
+    pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8888,debug=True)
