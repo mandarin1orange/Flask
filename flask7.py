1d0841009f622b44593a967a1579416e28b654fb
mandarin1orange <100086932+mandarin1orange@users.noreply.github.com>
2022-02-23 22:48:16 +0800

    URL别名
    # 小知识：flask.url_for('b1') 直接调用别名，不用写入函数名
    @app.route('/woshihenchangdeluyou',endpoint='b1')  # 别名

---
+++ flask7.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import flask
+from flask import Flask
+from flask import request
+from flask import json
+
+# 创建flask程序
+app = Flask(__name__,
+            static_url_path='/static',  # 静态文件路径
+            static_folder='static',
+            template_folder='templates'  # 模板文件
+            )
+
+@app.route('/woshihenchangdeluyou',endpoint='b1')  # 别名
+def b_page():
+    return '你来了啊'
+
+# 404重定向
+@app.errorhandler(404)
+def page_not_found(e):
+    return '你出错了',404
+
+@app.route('/a')
+def a_page():
+    return flask.redirect(flask.url_for('b1'))
+
+# 装饰器，关联路由
+@app.route('/')
+def index():
+    return 'haha'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8888,debug=True)
