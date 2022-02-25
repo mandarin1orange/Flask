97ebe4cce6fef378aba04ec77ad77bb3d16411b5
mandarin1orange <100086932+mandarin1orange@users.noreply.github.com>
2022-02-25 23:04:51 +0800

    控制代码块
    tempa.html
    
    <!DOCTYPE html>
    <html lang="en">
    <head>
        <meta charset="UTF-8">
        <title>a路由测试</title>
    </head>
    <body>
    <h1>i am father temp {{mint}} ---- {{mstr}}</h1>
    
    {% for item in mlist %}
    <li>{{item}}</li><hr>
    {% endfor %}
    
    {% for num in range(1,10) %}
    <li>{{num}} 号</li>
    {% endfor %}
    
    {% if vip == 0 %}
    <h1>你没充值</h1>
    {% elif vip == 1 %}
    <h1>lv1的内容</h1>
    {% endif %}
    
    </body>
    </html>

---
+++ flask13.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import flask
+from flask import *
+
+# 创建flask程序
+app = Flask(__name__,
+            static_url_path='/static',  # 静态文件路径
+            static_folder='static',
+            template_folder='templates'  # 模板文件
+            )
+
+@app.route('/woshihenchangdeluyou',endpoint='b1')
+def b_page():
+    return '你来了啊'
+
+@app.route('/a/<id>')
+def a_page(id):
+    # python参数进来都是string类型
+    m_int = int(id)+20
+    m_str = 'haha nihao'
+    m_list = ['xiaoming','xiaohong','xiaoli']
+    vip = 1
+    return flask.render_template('tempa.html',mint=m_int,mstr=m_str,mlist=m_list,vip=vip)
+
+# 装饰器，关联路由
+@app.route('/')
+def index():
+    return 'haha'
+
+# 404重定向
+@app.errorhandler(404)
+def page_not_found(e):
+    return '你出错了',404
+
+# 过滤器
+@app.template_filter('dore')
+def do_reverse(li):
+    temp = list(li)
+    temp.reverse()
+    return temp
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8888,debug=True)
